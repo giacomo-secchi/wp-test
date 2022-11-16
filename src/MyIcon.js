103b12601b4cfc78cd3d0bc68b2fd3fc75e7550b
giacomo.secchi <giacomosecchi@gmail.com>
2022-11-16 11:09:38 +0100

    Add orange ball icon

--- src/MyIcon.js
+++ src/MyIcon.js
@@ -3,45 +3,11 @@ import { Icon } from '@wordpress/components';
 export const MyIcon = () => (
 	<Icon
 		icon={ () => (
-			<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 1366 768" style="enable-background:new 0 0 1366 768;" xml:space="preserve">
-<style type="text/css">
-	.st0{fill:url(#SVGID_1_);}
-	.st1{fill:url(#SVGID_00000015337183475083509470000006575242181912232331_);}
-	.st2{fill:url(#SVGID_00000044139908438389994380000001886700002102580158_);}
-	.st3{fill:url(#SVGID_00000110444390760356438040000008342198596699990953_);}
-	.st4{opacity:0.56;fill:#FFA500;}
-</style>
-<g>
-	<g>
-		<linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="610.0724" y1="436.4489" x2="719.0707" y2="354.3127">
-			<stop  offset="0.2615" style="stop-color:#FFA500;stop-opacity:0.4"/>
-			<stop  offset="0.817" style="stop-color:#FFA500;stop-opacity:0"/>
-		</linearGradient>
-		<circle class="st0" cx="664.57" cy="395.38" r="68.24"/>
-		
-			<linearGradient id="SVGID_00000134932834967452963500000017876342296212220847_" gradientUnits="userSpaceOnUse" x1="588.3596" y1="435.3662" x2="699.5824" y2="351.5538">
-			<stop  offset="0.2615" style="stop-color:#FFA500;stop-opacity:0.4"/>
-			<stop  offset="0.817" style="stop-color:#FFA500;stop-opacity:0"/>
-		</linearGradient>
-		<circle style="fill:url(#SVGID_00000134932834967452963500000017876342296212220847_);" cx="643.97" cy="393.46" r="69.63"/>
-		
-			<linearGradient id="SVGID_00000023247029611600060620000015468585083118609075_" gradientUnits="userSpaceOnUse" x1="566.2037" y1="434.2614" x2="679.6963" y2="348.7386">
-			<stop  offset="0.2615" style="stop-color:#FFA500;stop-opacity:0.4"/>
-			<stop  offset="0.817" style="stop-color:#FFA500;stop-opacity:0"/>
-		</linearGradient>
-		<circle style="fill:url(#SVGID_00000023247029611600060620000015468585083118609075_);" cx="622.95" cy="391.5" r="71.05"/>
-		
-			<linearGradient id="SVGID_00000136381379484689582240000014437366848301159327_" gradientUnits="userSpaceOnUse" x1="543.5956" y1="433.1341" x2="659.4044" y2="345.8659">
-			<stop  offset="0.2615" style="stop-color:#FFA500;stop-opacity:0.4"/>
-			<stop  offset="0.817" style="stop-color:#FFA500;stop-opacity:0"/>
-		</linearGradient>
-		<circle style="fill:url(#SVGID_00000136381379484689582240000014437366848301159327_);" cx="601.5" cy="389.5" r="72.5"/>
-	</g>
-	<circle class="st4" cx="601.5" cy="389.5" r="72.5"/>
-</g>
-</svg>
-
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M24.5 20C24.5 22.4853 22.4853 24.5 20 24.5C17.5147 24.5 15.5 22.4853 15.5 20C15.5 17.5147 17.5147 15.5 20 15.5C22.4853 15.5 24.5 17.5147 24.5 20Z" fill="#FFDEC7" stroke="white"/>
+			<path d="M22.5 16C22.5 19.5899 19.5899 22.5 16 22.5C12.4101 22.5 9.5 19.5899 9.5 16C9.5 12.4101 12.4101 9.5 16 9.5C19.5899 9.5 22.5 12.4101 22.5 16Z" fill="#FFA361" stroke="white"/>
+			<path d="M20.5 10C20.5 15.799 15.799 20.5 10 20.5C4.20101 20.5 -0.5 15.799 -0.5 10C-0.5 4.20101 4.20101 -0.5 10 -0.5C15.799 -0.5 20.5 4.20101 20.5 10Z" fill="#FA6900" stroke="white"/>
+		</svg>
 		) }
 	/>
 );
